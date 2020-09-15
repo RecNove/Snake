047bb1228d0ef17a48e9035417f7d8abb0f2bc4c
RecNove <32557606+RecNove@users.noreply.github.com>
2020-09-14 21:08:09 -0300

    definindo teclas e adicionando loop

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-let jogo = setInterval(iniciarJogo, 100);
 let direction = "right";
 let snake = [];
 snake[0] = {
@@ -21,7 +20,21 @@ function criarCobrinha(){
   }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+  if(event.keyCode == 37 && direction != "right") direction = "left";
+  if(event.keyCode == 38 && direction != "up") direction = "down";
+  if(event.keyCode == 39 && direction != "left") direction = "right";
+  if(event.keyCode == 40 && direction != "down") direction = "up";
+}
+
 function iniciarJogo(){
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
+  if(snake[0].y < 0 && direction == "down") snake[0].y = 16 * box;
+
   criarBG();
   criarCobrinha();
 
@@ -44,4 +57,4 @@ function iniciarJogo(){
 }
 
 
-
+let jogo = setInterval(iniciarJogo, 100);
